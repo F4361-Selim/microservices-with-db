a19bf6deb9fd0496ae12074122dedf2b599492af
F4361-Selim <selim.mercimekk@gmail.com>
2023-02-18 06:53:39 +0000

    jenkinsfile deneme

--- jenkins/ecr-test-create.sh
+++ jenkins/ecr-test-create.sh
@@ -1,5 +1,5 @@
 PATH="$PATH:/usr/local/bin"
-APP_REPO_NAME="clarusway-repo/petclinic-app-dev" # Write your own repo name
+APP_REPO_NAME="clarusway-repo/petclinic-app-dev-1" # Write your own repo name
 AWS_REGION="us-east-1" #Update this line if you work on another region
 ECR_REGISTRY="211828209723.dkr.ecr.us-east-1.amazonaws.com" # Replace this line with your ECR name
 aws ecr create-repository \
